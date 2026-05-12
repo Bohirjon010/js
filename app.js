5bd1a271befe8a0b63cc7e2ae76611e463e4f742
Bohirjon010 <jsjdjh521@gmail.com>
2026-05-12 18:43:34 +0500

    zor

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const signupForm = document.querySelector("#signup-form");
 
 const message = document.querySelector(".message");
 
+const regEX = /^[a-zA-Z0-9]{6,12}$/;
+
 // kksksksksksks
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const regEX = /^[a-zA-Z0-9]{6,12}$/;
   const inputVal = signupForm.username.value;
 
   if (regEX.test(inputVal)) {
@@ -13,4 +14,14 @@ signupForm.addEventListener("submit", (e) => {
   } else {
     message.textContent = "Noto'g'ri";
   }
+});
+
+// live input
+signupForm.username.addEventListener("keyup", (e) => {
+if (regEX.test(e.target.value)){
+  signupForm.username.setAttribute('class','s')
+} else {
+  signupForm.username.setAttribute('class','e')
+  
+}
 });
